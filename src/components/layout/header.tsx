e5af848bbfe90d22fdb1bce6b5d69cffd6a222a7
Marcial Godes <mazinguer769@gmail.com>
2025-12-11 16:01:53 +0000

    Añade el efecto box-shadow (_for element span_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
               key={href}
               href={href}
               className={cn(
-                'relative px-3 py-2 transition-colors duration-300',
+                'group relative px-3 py-2 transition-all duration-300 rounded-md group-hover:shadow-lg group-hover:shadow-primary/20',
                 pathname === href ? 'text-primary' : 'text-foreground/60 hover:text-foreground'
               )}
             >
